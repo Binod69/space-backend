aead50cf46e912ad4a12841d3eed8e32ca8d4493
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-08-27 09:26:32 +0545

    added dotend and cors

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
+const cors = require('cors');
 
-const PORT = 3001;
+const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+const PORT = process.env.PORT ?? 3001;
 app.listen(PORT, (err) => {
   if (err) {
     console.log(`Error connecting to the server: ${err}`);
