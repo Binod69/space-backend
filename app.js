fc0c75c55f1abdaf6a0579f32eff35ac475382c9
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-08-27 10:16:11 +0545

    added error handlers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const errorMiddleware = require('./src/middleware/error.middleware');
+const CustomError = require('./custom.error');
 
 const app = express();
 app.use(express.json());
@@ -19,7 +21,18 @@ app.use(
 //ROUTES IMPORT START
 const routes = require('./routes/index');
 app.use('/api/v1', routes);
+//error handle
+app.all('*', (req, res, next) => {
+  const err = new CustomError(
+    `Can't find ${req.originalUrl} on the server!`,
+    404
+  );
+  next(err);
+});
 //ROUTES IMPORT ENDS
+
+//error middleware
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT ?? 3001;
 app.listen(PORT, (err) => {
